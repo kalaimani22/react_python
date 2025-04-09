40254f016632c55f572aae00b734a51ea913ad33
Kalaimani <kalaimani22raja@gmail.com>
2025-04-09 23:38:34 +0530

    Python build issue fix

--- react/src/App.js
+++ react/src/App.js
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   const [response, setResponse] = useState(null);
-  fetch("https://react-python-1ckc.onrender.com/api/items/").then(
-    (response) => {
-      return response.json();
-    }
-  ).then((res)=>setResponse(res));
+  useEffect(() => {
+    fetch("https://react-python-1ckc.onrender.com/api/items/")
+      .then((response) => {
+        return response.json();
+      })
+      .then((res) => setResponse(res));
+  } , []);
   return (
     <div className="App">
       <header className="App-header">
