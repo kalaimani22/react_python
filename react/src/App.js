81015d7ca5a5b657bd19fe1861d5d4e0c71e195f
Kalaimani <kalaimani22raja@gmail.com>
2025-04-09 08:51:51 +0530

    Re-added react folder as regular directory

---
+++ react/src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [response, setResponse] = useState(null);
+  fetch("http://127.0.0.1:8000/api/items/").then(
+    (response) => {
+      return response.json();
+    }
+  ).then((res)=>setResponse(res));
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {response && response.map((item) => (
+          <div key={item.id}>
+            <h2>{item.name}</h2>
+            <p>{item.description}</p>
+          </div>
+        ))}
+      </header>
+    </div>
+  );
+}
+
+export default App;
