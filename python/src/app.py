5bc360dfe28453a2cfab62a831e6878e105e5e76
Kalaimani <kalaimani22raja@gmail.com>
2025-04-09 23:15:29 +0530

    Python build issue fix

--- python/src/app.py
+++ python/src/app.py
@@ -4,6 +4,18 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+@app.get("/")
+def root():
+    return {"message": "FastAPI is running on Render!"}
+
+@router.get("/")
+def get_home():
+    return {"msg": "Hello from API"}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # or ["*"] for all origins (dev only)
